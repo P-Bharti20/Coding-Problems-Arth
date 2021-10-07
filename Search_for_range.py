8a33283fc017c1251c64e05fbfa0678518904857
P-Bharti20 <70098320+P-Bharti20@users.noreply.github.com>
2021-10-08 00:01:53 +0530

    Create Search_for_range.py

---
+++ Search_for_range.py
@@ -0,0 +1,5 @@
+   ---Search for Range---
+#A function that takes in a sorted array of integers as well as a target integer.The function should use a variation of the binary search algorithm to find a range of indices in between which the target number is contained in the array and should return  this range in the form of an array
+
+#The first number in the output array should represent the first index at which the target number is located, while the second number should represent the last index at which the target number is located. The function should return [ -1, -1 ] if the integer is not contained in the array
+
